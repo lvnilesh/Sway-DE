e1accb80a3d4b0cd25882926094ea363358bb0b8
Nilesh <nilesh@cloudgeni.us>
2024-05-31 13:09:19 -0700

    Mod1 Mod4 swap

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
-source ~/venv/bin/activate
+sudo pacman -S --needed --noconfirm -y python-pip ansible git curl wget less cowsay openssh btop bluez bluez-utils
 ansible-galaxy collection install community.general kewlfft.aur
 ansible-playbook setup-01-arch-prerequirements.yml -K -e 'ansible_python_interpreter=/usr/bin/python3' --diff
 ansible-playbook setup-02-de.yml -K -e 'ansible_python_interpreter=/usr/bin/python3' --diff
-# without venv
-# deactivate
-# yay -S auto-cpufreq
-# source ~/venv/bin/activate
 ansible-playbook setup-03-additions-arch.yml -K -e 'ansible_python_interpreter=/usr/bin/python3' --diff
