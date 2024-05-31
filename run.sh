b162909ea8240e727ede54faeb85282806effcd8
Nilesh <nilesh@cloudgeni.us>
2024-05-30 21:29:11 -0700

    ed

--- run.sh
+++ run.sh
@@ -1,22 +1,3 @@
-
-sudo pacman -S less git wget curl openssh cowsay python-pip btop
-
-
-yay -S bluez blueberry
-yay -S xdg-user-dirs
-
-sudo systemctl enable bluetooth
-sudo systemctl start bluetooth
-alacritty migrate
-
-python -m venv ~/venv
-source venv/bin/activate
-pip install ansible
-
-sudo pacman-key --init
-
-# copy ssh keys locally
-
 source ~/venv/bin/activate
 ansible-galaxy collection install community.general kewlfft.aur
 ansible-playbook setup-01-arch-prerequirements.yml -K -e 'ansible_python_interpreter=/usr/bin/python3' --diff
